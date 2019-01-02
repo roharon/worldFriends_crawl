369e47fa489bcea8d24b5bdb27430bac6ed20fa8
roharon <roharon98@gmail.com>
2019-01-02 20:37:26 +0900

    Bot_key => JSON file

--- bot_Send.py
+++ bot_Send.py
@@ -1,18 +1,25 @@
 import telegram
 #python-telegram-bot
 from crawl import crawl_wf
+import json
 
 def send_alert(sentence=None):
-    my_token = 'SECRET KEY'
+    my_token = ''
 
+    with open('./token.json', 'r') as f:
+        my_token = json.loads(f.read())["TOKEN"]
+    print(my_token)
     bot = telegram.Bot(token=my_token)
     updates = bot.getUpdates()
 
-    chat_id = bot.getUpdates()[-1].message.chat.id
+    try:
+        chat_id = bot.getUpdates()[-1].message.chat.id
+    except:
+        chat_id = 11
 
     # print(chat_id)
 
-    bot.send_message(chat_id=roharon, text='월프공지\n' + sentence)
+    bot.send_message(chat_id=chat_id, text='월프공지\n' + sentence.replace('\n', ' ').replace('*',''))
     #bot.send_message(chat_id=chat_id,text='월프공지')
 
 def send_telegram():
