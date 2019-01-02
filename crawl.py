89392cfbebf115426a0f165909428846124d5fd2
roharon <roharon98@gmail.com>
2019-01-02 20:37:58 +0900

    some fix

--- crawl.py
+++ crawl.py
@@ -3,25 +3,25 @@ from bs4 import BeautifulSoup
 import lxml
 
 def crawl_wf():
-    req = requests.get('https://kiv.nia.or.kr/front/sptr_news/noticeList.do')
+    req = requests.get('http://kiv.nia.or.kr/front/sptr_news/noticeList.do')
 
     html = req.text
-
+    #print(html)
     soup = BeautifulSoup(html,'lxml')
 
     title = soup.find_all('tr', class_='top')
+    #print(title)
 
     article = []
     for i in title:
         article.append(i.text)
 
-    #print(article)
+    print(article)
 
     if '2018/05/16' not in article[0]:
         return [1,article[0]]
     else:
         [0,'']
-
 
 
 if __name__ == '__main__':
